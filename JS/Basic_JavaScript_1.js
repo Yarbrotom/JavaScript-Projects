b53e02d202e944fec9e5c49ffd997218493000a5
Thomas Yarbrough <yarbrotom@gmail.com>
2020-06-16 11:25:23 -0700

    Add first Java script assignment
    Add html and js files for first assignment

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+
+window.alert("This is an alert."); // Use a window alert
+var A ="This isn\'t a string" + ", no really.", B ="yay"; //Assign a variable, use and escape character and concatonate, assign a second variable
+var A = A.fontcolor("blue"); // Change font color of some text.
+
+document.write(3 + 4) // Use and expression.
+
+document.write(A + B); // Display the variable on the screen.
